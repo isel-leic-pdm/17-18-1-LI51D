030a3881a25bc900a2c4ca1b93ae003cc82b3421
Paulo Pereira <palbp.giga@gmail.com>
2017-11-14 20:43:13 +0000

    Added code sample used to listen to explicit broadcast intents

--- BroadcastReceivers101/app/src/main/java/pdm/isel/broadcastreceivers101/MainActivity.kt
+++ BroadcastReceivers101/app/src/main/java/pdm/isel/broadcastreceivers101/MainActivity.kt
@@ -1,12 +1,26 @@
 package pdm.isel.broadcastreceivers101
 
+import android.content.IntentFilter
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
+    val receiver = MyReceiver()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val filter = IntentFilter("android.net.conn.CONNECTIVITY_CHANGE")
+        registerReceiver(receiver,filter)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        unregisterReceiver(receiver)
     }
 }
