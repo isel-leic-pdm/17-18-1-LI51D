ac296a6d29a95dcedd269777084afffb6378ee9d
Paulo Pereira <palbp.giga@gmail.com>
2017-12-05 12:45:32 +0000

    Added demo code for System Service's access

--- SystemServices101/app/src/main/java/pdm/isel/systemservices101/MainActivity.kt
+++ SystemServices101/app/src/main/java/pdm/isel/systemservices101/MainActivity.kt
@@ -1,6 +1,7 @@
 package pdm.isel.systemservices101
 
 import android.app.AlarmManager
+import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
@@ -10,21 +11,33 @@ import android.util.Log
 import android.app.job.JobInfo
 import android.content.ComponentName
 import android.app.job.JobScheduler
-
+import android.support.v7.app.NotificationCompat
 
 
 class MainActivity : AppCompatActivity() {
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val am = getSystemService(Context.ALARM_SERVICE) as AlarmManager
-        val pi = PendingIntent.getService(this, 1001, Intent(this, MyIntentService::class.java), 0)
-        am.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, 5*60*1000, pi)
-        Log.v("SystemServices101", "onCreate ends")
+        // demoAlarmManager()
+        // demoJobScheduler()
+        demoNotificationManager()
+    }
 
+    private fun demoNotificationManager() {
+        val mBuilder = NotificationCompat.Builder(this)
+                .setSmallIcon(R.mipmap.ic_launcher_round)
+                .setContentTitle("My notification")
+                .setContentText("Hello World!")
 
+        val notificationMgr = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationMgr.notify(11111, mBuilder.build())
+    }
+
+    private fun demoJobScheduler() {
         val builder = JobInfo.Builder(MyJobService.JOB_ID,
                 ComponentName(this, MyJobService::class.java))
 
@@ -32,5 +45,12 @@ class MainActivity : AppCompatActivity() {
 
         val jobScheduler = getSystemService(Context.JOB_SCHEDULER_SERVICE) as JobScheduler
         jobScheduler.schedule(builder.build())
+    }
+
+    private fun demoAlarmManager() {
+        val am = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        val pi = PendingIntent.getService(this, 1001, Intent(this, MyIntentService::class.java), 0)
+        am.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, 5 * 60 * 1000, pi)
+        Log.v("SystemServices101", "onCreate ends")
     }
 }
